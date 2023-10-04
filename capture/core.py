31c6bbde180c392d46367bec351c8ed9672e5832
Logic <fanyongda2012@gmail.com>
2023-10-04 15:38:39 -0400

    recognize score

--- capture/core.py
+++ capture/core.py
@@ -1,6 +1,12 @@
-import pyautogui
-import cv2
 from PIL import Image
+import pyautogui
+import pytesseract
+import cv2
+import numpy
+
+pytesseract.pytesseract.tesseract_cmd = (
+    "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+)
 
 
 # get the full screen image
@@ -16,8 +22,14 @@ def screen_image() -> Image:
 
 # given a full screen image, return the current score
 def score(image: Image) -> int:
-    image.crop((72, 100, 313, 170)).show()
+    image = numpy.array(image.crop((72, 115, 313, 165)))
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    _, image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    text = pytesseract.image_to_string(
+        image, lang="eng", config="--psm 8 -c tessedit_char_whitelist=0123456789"
+    )
+    return int(text)
 
 
 screen = screen_image()
-score(screen)
+print(score(screen))
