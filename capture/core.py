c7f32028d529af0d905dbabc4e24e144d6c4212c
Logic <fanyongda2012@gmail.com>
2023-10-04 16:33:33 -0400

    add drop position

--- capture/core.py
+++ capture/core.py
@@ -11,25 +11,37 @@ pytesseract.pytesseract.tesseract_cmd = (
 
 # get the full screen image
 def screen_image() -> Image:
-    position = pyautogui.locateOnScreen("resource/locate.png", confidence=0.95)
-    if position is None:
+    pos = pyautogui.locateOnScreen("resource/locate.png", confidence=0.95)
+    if pos is None:
         return None
-    x = position.left + position.width - 1280
-    y = position.top + position.height - 720
+    x = pos.left + pos.width - 1280
+    y = pos.top + pos.height - 720
     screenshot = pyautogui.screenshot(region=(x, y, 1280, 720))
     return screenshot
 
 
 # given a full screen image, return the current score
-def score(image: Image) -> int:
-    image = numpy.array(image.crop((72, 115, 313, 165)))
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    _, image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+def score(img: Image) -> int:
+    img = numpy.array(img.crop((72, 115, 313, 165)))
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     text = pytesseract.image_to_string(
-        image, lang="eng", config="--psm 8 -c tessedit_char_whitelist=0123456789"
+        img, lang="eng", config="--psm 8 -c tessedit_char_whitelist=0123456789"
     )
     return int(text)
 
 
+def playground(img: Image) -> Image:
+    return img.crop((404, 0, 876, 687))
+
+
+# return a value between 0 and 327
+def drop_position(img: Image) -> int:
+    pos = pyautogui.locate("resource/drop.png", img, confidence=0.8)
+    pos = pos.left - 491
+    return pos
+
+
 screen = screen_image()
-print(score(screen))
+playground(screen)
+drop_position(screen)
