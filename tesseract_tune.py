997feb393840d945c42e3706ce386f30accb7eb5
Logic <fanyongda2012@gmail.com>
2023-10-05 02:40:28 -0400

    update crop method

--- tesseract_tune.py
+++ tesseract_tune.py
@@ -15,13 +15,13 @@ os.makedirs("data/num-ground-truth/", exist_ok=True)
 def write(img: Image, score: int):
     global count
 
-    img = numpy.array(img.crop((72, 115, 313, 165)))
+    img = numpy.array(img.crop((112, 115, 273, 165)))
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
     Image.fromarray(img).save(f"data/num-ground-truth/{count:0>5}.png")
-    with open(f"data/num-ground-truth/{count:0>5}.gt.txt", "w") as f:
-        f.write(str(score))
+    # with open(f"data/num-ground-truth/{count:0>5}.gt.txt", "w") as f:
+    #     f.write(str(score))
 
     count += 1
 
