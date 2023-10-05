7771354ddf759e80abed54a65a156c127def0d06
Logic <fanyongda2012@gmail.com>
2023-10-04 20:23:41 -0400

    fixup

--- preprocess.py
+++ preprocess.py
@@ -5,5 +5,5 @@ import os
 #     images = sorted(os.listdir(f"data/raw/{run}"), key=lambda x: int(x[:-4]))
 #     print(images)
 
-for i in range(31):
+for i in range(31, 52):
     os.rename(f"data/raw/202310041908/{i:>3}.png", f"data/raw/202310041908/{i:0>3}.png")
