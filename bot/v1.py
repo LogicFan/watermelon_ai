3ecb7a6b891c4fe0eb23c10f086fce83a2a94d3b
Logic <fanyongda2012@gmail.com>
2023-10-05 12:42:44 -0400

    update model

--- bot/v1.py
+++ bot/v1.py
@@ -13,7 +13,7 @@ class BotV1(core.BaseBot):
         model.classifier[3] = torch.nn.Linear(
             in_features=1024, out_features=1, bias=True
         )
-        model.load_state_dict(torch.load("./model_20231005_025025_2"))
+        model.load_state_dict(torch.load("./model_20231005_123209_1"))
         self.model = model
         self.model.eval()
 
